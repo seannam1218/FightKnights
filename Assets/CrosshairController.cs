1089310b0c40adefc06133e4097800caf76fdd35
Sean Nam <seannam@protonmail.com>
2020-04-27 21:54:48 -0700

    implement crosshair

--- Assets/CrosshairController.cs
+++ Assets/CrosshairController.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
 
-public class CrosshairController : NetworkBehaviour
+public class CrosshairController : MonoBehaviour
 {
+	Vector3 mousePos;
+	Vector3 mouseWorldPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10);
-        transform.position = mousePos;
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, mousePos.z+10));
+
+        this.transform.position = mouseWorldPos;
     }
 }
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public static CameraController Instance;
+
+    public GameObject Target;
+    public int smoothValue =2;
+    public float yOffset = (float) -0.5;
+    public Vector3 mousePos;
+    public float pullValue = 10;
+
+    // Use this for initialization
+    public Coroutine my_co;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10);
+  
+        Vector3 targetPos = new Vector3(
+        Target.transform.position.x + (mousePos.x/Screen.width - (float)0.5) * pullValue, 
+        Target.transform.position.y + yOffset + (mousePos.y/Screen.height - (float)0.5) * pullValue, 
+        -10);
+
+        transform.position = targetPos;
+        // transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime*smoothValue);
+    }
+
+
+
+}
